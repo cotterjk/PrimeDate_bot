150eb8563dfa61134a1a51de744fb0746a214606
Cotter Koopman <Cotter@cotters-air.attlocal.net>
2017-07-03 15:54:37 -0400

    Trying hello world

--- primedate.py
+++ primedate.py
@@ -2,6 +2,7 @@ import random
 import re
 import sys
 import twitter
+import time
 from local_settings import *
 
 def connect():
@@ -11,6 +12,32 @@ def connect():
                           access_token_secret=MY_ACCESS_TOKEN_SECRET)
     return api
 
+def getAllFactorsFor(remainder):
+    factors = []
+    for i in range(2, remainder, 1):
+        while ((remainder % i) == 0):
+            factors.append(i)
+            remainder /= i
+    return factors
+
+def getStringFromFactors(factors):
+    factorstr = ''
+    if len(factors) == 1:
+        factorstr = 'prime. Cool!'
+    else:
+        for i in range(len(factors)):
+            factorstr += str(factors[i])
+            if i != (len(factors)-1):
+                factorstr += ' x '
+        factorstr += '.'
+    return factorstr
+
 if __name__=="__main__":
     api=connect()
-    api.PostUpdate('Hello twitter take 2')
+
+    date = time.strftime("%m/%d/%y")
+    dateint = int(time.strftime("%m%d%y"))
+
+    tweet = 'Today is ' + date + '. ' + dateint + ' is ' + getStringFromFactors(getAllFactorsFor(dateint))
+    print (tweet)
+    api.PostUpdate(tweet)
